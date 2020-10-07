d76f3b16fb1565bc372b59fefdd62396985e5922
TahanyAljemzawy <tahanyaljemzawy@yahoo.com>
2020-10-07 09:46:38 +0300

    working on functionality

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # Hello-world-
 just start - new repo
-this is educational web page about some of the simple subject at the html
+this is educational web page about some simple subject at the html
 contains some informations and a interactive questions
 
 it well be good to the beginner at progrmmaing , and as at every language you will learn, you well see
 example using the sentince Hello World and from there I take the name ..
 
 Tahany todo list for this proj:
+<today gools>
+	done//1- fix the bring content function so it do all the styles and contenet to the page
+	new prop: when press ever you click the subj it will be created again ! T_T
+	done//2- improve the empty content function
+	    ==> U have 2h for that
+	3-break 15m if i finish this target
+	4- after 25m break 5m (move yur mascels)
+	5-finish the contant info 3h or 4h and fix the position of the white space plz
+	6-nab for 30m or 45m
+	7- start the exersise section writing questions at least 3 questions for each subj
+
+
 1- you need to make the html file with the proper id you have 1 ot 2 houer 
 (search how to make a list or drobdown minu)//done 
 2-use the bomo app and take arest 5 m & strech your self ..
--- script.js
+++ script.js
@@ -1,60 +1,64 @@
 $(document).ready(function(){
 
 //this function is called when ever you click on the subject at the menu
-//brings the data from the obj and show it in its right place
+//brings the data from the array of obj and show it in its right place
   function bringContent(obj) {
-  	var title = obj.title;
-  	//$("#whiteSpace").append("<div>"+title+"</div>");
-  	
-  	$("#whiteSpace").append("<h1>"+title+"</h1>");
+    //take the id of the page
+    var subjectId = obj.subjId;
+  
+  //creat the section that will contain the informations(p,h)
+    $("#whiteSpace").append("<div>"+"."+"</div>");
+    $("#whiteSpace div").addClass("info , subjectId");
+
+    //take the title and append it
+    var title = obj.title;
+    $(".info").append("<h1>"+title+"</h1>");
+    $(".info h1").addClass("subjHeader");
 
   	//loop at the obj to set the paragrah to the page exapt title and color no need
    	for(var key in obj) {
 
-   		if(key === "title" || key === "background")
+   		if(key === "title" || key === "subjId")
    			continue;
-
    		else {
    			//incase we have secound title to give it the title decoration 
    			if(key === "title2" || key === "title3" || key === "title4")
-   			$("#whiteSpace").append("<h2>"+obj[key]+"</h2>");
+   			$(".info").append("<h2>"+obj[key]+"</h2>");
    			else
-   			$("#whiteSpace").append("<p> <XMP>"+obj[key]+"</XMP> </p>");	
+   			$(".info").append("<p> <XMP>"+obj[key]+"</XMP> </p>");	
    		}
   	
-  	if(obj.backgroundColor !== ""){
+ /* 	if(obj.backgroundColor !== ""){
   		var backgroundColor = obj.background
-  		console.log(backgroundColor);
-  		$("#info").css("background-color",backgroundColor);
-  	}
+  		$(".info").css("background-color",backgroundColor);
+  	}*/
   	
   }}//the end of function bring content
 
+//this function clear the info of the section to set the new one
   function emptyTheContent() {
-  	$("#info").remove();
-
-
+   	$(".info").remove();
   }
 
 //subj0 click function call the bring content function and giv it his obj
   $("#subj0").click(function() {
-
-    bringContent(arrayOfContants[0])
-    
+    //if()
+    bringContent(arrayOfContants[0])//create the contant of this subj
+    var backgroundColor = arrayOfContants[0].background;
+    $(".info").css("background-color",backgroundColor);
   });
+
   $("#subj1").click(function() {
   	emptyTheContent();
   });
 //the array of obj each opj is for one page
 var arrayOfContants = [{
 	title:"Introduction To HTML",
-	p1:"HTML is the standard markup language for Web pages With HTML you can create your own Website.HTML is easy to learn \n- You will enjoy it!",
-    p2:"HTML describes the structure of a Web page, HTML elements tell the browser how to display the content",
-    p3:'HTML elements label pieces of content such as "this is a heading", "this is a paragraph", "this is a link", etc.',
-    title2:"What is an HTML Element?",
-    p5:"An HTML element is defined by a start tag, some content, and an end tag:\n <XMP><tagname>Content goes here...</tagname></XMP>\n The HTML element is everything from the start tag to the end tag: ",
-    p6:"<h1>My First Heading</h1> \n <p>My first paragraph.</p>",
-    background:"#98FB98" ,
-    },]
+  subjId : "subj0",
+	p1:"HTML is the standard markup language for Web pages With\n HTML you can create your own Website.HTML is easy to learn \n- You will enjoy it! \n HTML describes the structure of a Web page,\n HTML elements tell the browser how to display the content\n'HTML elements label pieces of content such as \n this is a heading this is a paragraph \n this is a link etc.",
+  title2:"What is an HTML Element?",
+  p2:"An HTML element is defined by a start tag, some content, and an end tag:\n <tagname>Content goes here...</tagname></XMP>\n The HTML element is everything from the start tag to the end tag:  <h1>My First Heading</h1> \n <p>My first paragraph.</p>",
+  background:"#98FB98" ,
+ },]
 
 });//The ready end
